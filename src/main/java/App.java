d1d786b011e1afc69ec2524ba71a8cb979d858c4
Ubuntu <ubuntu@ip-172-31-30-37.ec2.internal>
2026-04-07 23:22:16 +0000

    feat: adiciona app java e estrutura maven

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "CD funcionando - Java";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Servidor rodando na porta 8080...");
+    }
+}
