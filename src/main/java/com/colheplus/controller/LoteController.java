6e4b28734cba4dcfc84c0e73dacdd0481800e644
Joao Pedro Pioltini <joaopioltini22@gmail.com>
2026-04-28 20:36:57 -0300

    feature correction ci

--- src/main/java/com/colheplus/controller/LoteController.java
+++ src/main/java/com/colheplus/controller/LoteController.java
@@ -2,7 +2,10 @@ package com.colheplus.controller;
 
 import com.colheplus.model.Lote;
 import com.colheplus.service.LoteService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
--- src/main/java/com/colheplus/controller/PedidoController.java
+++ src/main/java/com/colheplus/controller/PedidoController.java
@@ -3,7 +3,10 @@ package com.colheplus.controller;
 import com.colheplus.model.Pedido;
 import com.colheplus.service.PedidoService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
